9c5974bf52cb221e79e50d664a9a11134cb0dc46
Nada Elsnosy <nadaalsnosy@gmail.com>
2021-09-21 14:48:04 +0200

    add remove

--- Task-4/todo/src/app/app.component.ts
+++ Task-4/todo/src/app/app.component.ts
@@ -7,71 +7,58 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  todos = [
+  todos: { title: string, id: number, completed: boolean }[] = [
     {
-      "userId": 1,
       "id": 1,
       "title": "delectus aut autem",
       "completed": false
     },
     {
-      "userId": 1,
       "id": 2,
       "title": "quis ut nam facilis et officia qui",
       "completed": false
     },
     {
-      "userId": 1,
       "id": 3,
       "title": "fugiat veniam minus",
       "completed": false
     },
     {
-      "userId": 1,
       "id": 4,
       "title": "et porro tempora",
       "completed": true
     },
     {
-      "userId": 1,
       "id": 5,
       "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
       "completed": false
     },
     {
-      "userId": 1,
       "id": 6,
       "title": "qui ullam ratione quibusdam voluptatem quia omnis",
       "completed": false
     },
     {
-      "userId": 1,
       "id": 7,
       "title": "illo expedita consequatur quia in",
       "completed": false
     },
     {
-      "userId": 1,
       "id": 8,
       "title": "quo adipisci enim quam ut ab",
       "completed": true
     },
     {
-      "userId": 1,
       "id": 9,
       "title": "molestiae perspiciatis ipsa",
       "completed": false
     },
     {
-      "userId": 1,
       "id": 10,
       "title": "illo est ratione doloremque quia maiores aut",
       "completed": true
     },
   ]
-
-
-
 
 
   isContainer = true;
@@ -95,14 +82,12 @@ export class AppComponent {
     }
   }
 
-
   isList = true;
   list() {
     return {
       list: this.isList
     }
   }
-
 
   isLi = true;
   li() {
@@ -122,15 +107,25 @@ export class AppComponent {
 
 
   todoItem = '';
-
   addTodo(input: HTMLInputElement) {
-    this.todoItem = input.value
+    this.todoItem = input.value;
+    this.todos.push({
+      "id": new Date().getTime(),
+      "title": this.todoItem,
+      "completed": false
+    })
+    this.todoItem = ' ';
+    console.log(input.value)
   }
 
-  
 
-  removeTodo(li: HTMLInputElement) {
-    li.remove()
+
+  removeTodo(id: number) {
+    const todoIndex = this.todos.findIndex(todo => todo.id == id);
+    if (todoIndex > -1) {
+      this.todos.splice(todoIndex, 1)
+    }
+    console.log(todoIndex);
   }
 
 
